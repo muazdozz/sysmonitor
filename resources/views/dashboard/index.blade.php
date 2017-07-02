6170db9e183d04ae05c338db395b9a15a24d1b41
muazdozz <muaz16@gmail.com>
2017-07-02 12:23:06 +0530

    first commit

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,17 +2,24 @@
 @section('content')
 <div class="container">
 @foreach($sites as $site)
-	<div class="tile">
+{{-- 	<div class="tile">
       <div class="tile is-parent is-vertical">
         <article class="tile is-child notification is-primary">
           <p class="title">{{ $site->name }}</p>
-          <p class="subtitle">Top tile</p>
+          <p class="">{{ $site->address }}</p>
+
         </article>
         <article class="tile is-child notification is-warning">
-          <p class="title">...tiles</p>
-          <p class="subtitle">Bottom tile</p>
+          <p class="title">{{ $site->name }}</p>
+          <p class="">{{ $site->address }}</p>
         </article>
-      </div>
+      </div> --}}
+  <div class="card tile is-parent is-vertical">
+    <div class="card-image tile is-child notification is-primary""> 
+        <p class="title">{{ $site->name }}</p>
+        <p class="">{{ $site->address }}</p> 
+    </div>
+</div>
 
 @endforeach
 
--- resources/views/modules/nav.blade.php
+++ resources/views/modules/nav.blade.php
@@ -18,12 +18,6 @@
       <a class="nav-item is-tab is-hidden-tablet">Features</a>
       <a class="nav-item is-tab is-hidden-tablet">Pricing</a>
       <a class="nav-item is-tab is-hidden-tablet">About</a>
-      <a class="nav-item is-tab">
-        <figure class="image is-16x16" style="margin-right: 8px;">
-          <img src="http://bulma.io/images/jgthms.png">
-        </figure>
-        Profile
-      </a>
       <a class="nav-item is-tab">Log out</a>
     </div>
   </div>
